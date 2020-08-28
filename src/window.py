e0381533519f39c088c54ba2915ce3ed9fa5b48b
Rafael Mardojai CM <mardojai.cardenas@gmail.com>
2020-08-27 22:49:10 -0500

    Rework widgets 2

--- src/window.py
+++ src/window.py
@@ -15,35 +15,73 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import Gtk, Gio, Handy
+from gi.repository import Gtk, Handy
 
-from .sound import Sound, SoundWidget
+from .sound import SoundObject
+from .widgets import SoundsGroup
 
 
 @Gtk.Template(resource_path='/com/rafaelmardojai/Blanket/window.ui')
 class BlanketWindow(Handy.ApplicationWindow):
     __gtype_name__ = 'BlanketWindow'
 
-    default_list = Gtk.Template.Child()
+    box = Gtk.Template.Child()
 
-    sounds = [
-        'Rain',
-        'Wind'
-    ]
+    sounds = {
+        'Nature': [
+            'Rain',
+            'Wind',
+            'Summer Night'
+        ],
+        'Interiors': [
+            'Coffee Shop',
+            'Fireplace'
+        ],
+        'Noise': [
+            'Pink Noise',
+            'White Noise'
+        ]
+    }
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.model = Gio.ListStore.new(Sound)
-        self.default_list.bind_model(self.model, self._create_sound_widget)
+        self.setup_sounds()
+        self.setup_custom_sounds()
 
-        for s in self.sounds:
-            sound = Sound(s)
-            self.model.append(sound)
+        self.show_all()
 
-        self.default_list.show_all()
+    def setup_sounds(self):
+        # Setup default sounds
+        for g, sl in self.sounds.items():
+            # Create a new SoundsGroup
+            group = SoundsGroup(g)
+            # Iterate sounds
+            for s in sl:
+                # Create a new SoundObject
+                sound = SoundObject(s)
+                # Add SoundObject to SoundsGroup
+                group.add(sound)
 
-    def _create_sound_widget(self, sound):
-        widget = SoundWidget(sound)
-        return widget
+            # Add SoundsGroup to the window's main box
+            self.box.pack_start(group, False, True, 0)
+
+    def setup_custom_sounds(self):
+        # Setup user custom sounds
+        self.custom_sounds = SoundsGroup('Custom')
+        self.box.pack_start(self.custom_sounds, False, True, 0)
+
+        # Add sound button row
+        add_row = Gtk.ListBoxRow()
+        add_row.set_selectable(False)
+        add_row_box = Gtk.Box(
+            height_request = 34
+        )
+        add_row.add(add_row_box)
+        add_row_icon = Gtk.Image.new_from_icon_name(
+            'list-add-symbolic',
+            Gtk.IconSize.MENU
+        )
+        add_row_box.pack_start(add_row_icon, True, True, 0)
+        self.custom_sounds.listbox.add(add_row)
 
