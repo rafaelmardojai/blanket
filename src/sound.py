11e47dc3f01de985a00ff167817314168c973123
Rafael Mardojai CM <mardojai.cardenas@gmail.com>
2020-09-03 10:53:21 -0500

    Rework GStreamer code

--- src/sound.py
+++ src/sound.py
@@ -15,86 +15,92 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import GObject, Gst, Gtk, Handy
+from gi.repository import GObject, GstPlayer
 
-'''
-SoundObject
-Describe a sound with it's propeties
-'''
+
+class MainPlayer(GObject.GObject):
+    '''
+    Virtual app sounds player
+    '''
+
+    __gsignals__ = {
+        'volume-changed': (GObject.SignalFlags.RUN_FIRST, None, (int,)),
+        'muted': (GObject.SignalFlags.RUN_FIRST, None, (bool,)),
+    }
+
+    def __init__(self):
+        super().__init__()
+
+        self.volume = 1
+
+    def set_muted(self, muted):
+        self.emit('muted', muted)
+
+    def set_volume(self, volume):
+        self.volume = volume
+        self.emit('volume-changed', volume)
+
+
 class SoundObject(GObject.Object):
+    '''
+    Describe a sound with it's propeties
+    '''
 
-    def __init__(self, name, uri=None, title=None, icon=None, removable=False, **kwargs):
+    def __init__(self, name, uri=None, title=None, icon=None, removable=False, mainplayer=None, **kwargs):
         super().__init__(**kwargs)
 
         resource_tmpl = 'resource:////com/rafaelmardojai/Blanket/sounds/{}.ogg'
         icon_tmpl = 'com.rafaelmardojai.Blanket-{}'
 
-        self.removable = removable
         self.name = name
         self.uri = uri if uri else resource_tmpl.format(self.name)
         self.title = title if title else name
         self.icon_name = icon if icon else icon_tmpl.format(self.name)
+        self.removable = removable
+        self.mainplayer = mainplayer
 
 
-'''
-SoundPlayer
-Create and controll a GStreamer playbin with the given Sound
-
-Based on Focusli JavaScript code
-https://github.com/armonge/gnome-shell-extension-focusli
-Copyright (C) 2017 Felipe Borges <felipeborges@gnome.org>
-
-Ported to Python by Rafael Mardojai CM
-'''
-class SoundPlayer(object):
+class SoundPlayer(GstPlayer.Player):
+    '''
+    GstPlayer.Player with modifications
+    '''
 
     def __init__(self, sound):
-        self.sound = sound
+        super().__init__()
+        # Alwas start with volume in 0
+        self.set_volume(0)
 
-        self.playbin = Gst.ElementFactory.make('playbin', self.sound.name)
-        self.playbin.set_property('uri', self.sound.uri)
+        # Set SoundObject.uri as player uri
+        self.set_uri(sound.uri)
+        self.name = sound.name
 
-        self.prerolled = False
-        playbin_bus = self.playbin.get_bus()
-        playbin_bus.add_signal_watch()
-        playbin_bus.connect("message", self._on_bus_message)
+        # Connect mainplayer muted signal
+        sound.mainplayer.connect('muted', self._on_mute)
 
-    def play(self):
-        self.playbin.set_state(Gst.State.PLAYING)
-
-    def pause(self):
-        self.playbin.set_state(Gst.State.PAUSED)
-
-    def set_volume(self, value):
-        self.playbin.set_property('volume', value)
-        state = self.playbin.get_state(Gst.State.NULL)
-
-        if value == 0:
-            self.pause()
-        elif state is not Gst.State.PLAYING:
-            self.play()
+        # Connect volume-changed signal
+        self.connect('volume-changed', self._on_volume_changed)
+        # Connect end-of-stream signal
+        self.connect('end-of-stream', self._on_eos)
 
     def remove(self):
-        self.playbin.set_state(Gst.State.NULL)
-        self.playbin.unref()
+        # Stop player
+        self.stop()
+        # Unref player
+        self.unref()
 
-    def _on_bus_message(self, bus, message):
-        if message:
-            if message.type is Gst.MessageType.SEGMENT_DONE:
-                self.playbin.seek_simple(
-                    Gst.Format.TIME,
-                    Gst.SeekFlags.SEGMENT,
-                    0
-                )
+    def _on_mute(self, player, mute):
+        self.set_mute(mute)
 
-            if message.type is Gst.MessageType.ASYNC_DONE:
-                if not self.prerolled:
-                    self.playbin.seek_simple(
-                        Gst.Format.TIME,
-                        Gst.SeekFlags.FLUSH | Gst.SeekFlags.SEGMENT,
-                        0
-                    )
-                    self.prerolled = True
 
-            return True
+    def _on_volume_changed(self, player):
+        # Always play if volume > 0
+        volume = self.get_volume()
+        if volume == 0:
+            self.pause()
+        else:
+            self.play()
+
+    def _on_eos(self, player):
+        # Seek player to 0
+        self.seek(0)
 
