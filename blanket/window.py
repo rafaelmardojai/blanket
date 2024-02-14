a543c4c764f0d273394602d8da10d67271ccef1d
Dexter Reed <dreed4470@proton.me>
2024-02-14 09:08:50 +0000

    feat: Port dialogs to Adw.Dialog, ...
    Add `.flatpak-builder` to .gitignore
    
    Port `Adw.MessageDialog`s to `Adw.AlertDialog`
    
    Change homepage link in metainfo and about to the `apps.gnome.org` link,
    we already have vcs-browser, and I added a custom source code link in
    the about dialog.
    
    Port `Adw.AboutWindow` to `Adw.AboutDialog`
    
    Port `Adw.PreferencesWindow` to `Adw.PreferencesDialog`
    
    Update `blueprint-compiler` to the latest version

--- blanket/window.py
+++ blanket/window.py
@@ -129,18 +129,17 @@ class BlanketWindow(Adw.ApplicationWindow):
             else:
                 Settings.get().remove_custom_audio(name)
 
-                message = Adw.MessageDialog.new(
-                    self,
+                alert = Adw.AlertDialog.new(
                     _('Sound Automatically Removed'),
                     _(
                         'The {name} sound is no longer accessible, so it has been removed'
                     ).format(name=f'<b><i>{name}</i></b>'),
                 )
-                message.add_response('accept', _('Accept'))
-                message.props.body_use_markup = True
-                message.props.default_response = 'accept'
-                message.props.close_response = 'accept'
-                message.present()
+                alert.add_response('accept', _('Accept'))
+                alert.props.body_use_markup = True
+                alert.props.default_response = 'accept'
+                alert.props.close_response = 'accept'
+                alert.present(self)
 
     def open_audio(self):
         def on_response(_filechooser, _id):
@@ -179,6 +178,7 @@ class BlanketWindow(Adw.ApplicationWindow):
         self.filechooser = Gtk.FileChooserNative.new(  # type: ignore
             _('Open audio'), self, Gtk.FileChooserAction.OPEN, None, None
         )
+        self.filechooser.set_modal(True)
         self.filechooser.connect('response', on_response)
 
         for f, mts in filters.items():
